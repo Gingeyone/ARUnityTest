b9b2109192e22c665bd8e63f3e5f3d40895bbc80
Adam Hastings <43536118+Gingeyone@users.noreply.github.com>
2022-10-31 17:31:55 +0000

    Initial Commits
    -Assets folder
    -Includes Cartoon Girl model that will be placed in the scene
    -2D Indicator textures and materials
    -Script to tap to place an object on a horizontal plane

---
+++ Assets/Scripts/AR_TapToPlaceObject.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+using UnityEngine.XR.ARSubsystems;
+
+public class AR_TapToPlaceObject : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager aRRaycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    
+    void Update()
+    {
+
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+
+            PlaceObject();
+        }
+
+        
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    //Update placment Indicator to show if In can drop an object
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    //Check if i can place an object
+    private void UpdatePlacementPose() 
+    {
+        var screenCentre = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCentre, hits, TrackableType.Planes);
+        
+        placementPoseIsValid = hits.Count > 0;
+
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBaring = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+
+            placementPose.rotation = Quaternion.LookRotation(cameraBaring);
+        }
+
+    }
+}
