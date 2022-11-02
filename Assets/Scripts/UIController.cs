ef3ae886489867f02bd89ae72213a1d62ee6a0c9
Adam Hastings <43536118+Gingeyone@users.noreply.github.com>
2022-11-02 08:33:08 +0000

    UI FIX
    Features Changed
    -UI has been fixed to iPhone8 resolution
    -Working on getting the debug text for distance working
    -Animations altered for new resolutions

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,12 +5,19 @@ using TMPro;
 using UnityEditor;
 using UnityEngine.UI;
 using System;
+using TMPro;
+using UnityEditor;
+using UnityEngine.UI;
+using System;
 
 public class UIController : MonoBehaviour
 {
 
     public Animator anim_eyeUIControl; // Camera Window
     public Animator anim_shoeUIControl; // Camera Window
+
+    public TMP_Text tMP_Text;
+    public DistanceCheckingForWalking distanceCheckingForWalking;
 
     public void Btn_eyeButton()
     {
@@ -40,6 +47,14 @@ public class UIController : MonoBehaviour
         }
 
     }
+
+    public void Update ()
+    {
+        //Update the debug UI to show the distance between the camera and the Model
+
+        tMP_Text.text = "Current Distance: " + distanceCheckingForWalking.currentDistanceRounded;
+    }
+
 
 
 }
